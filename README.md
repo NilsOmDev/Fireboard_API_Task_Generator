7b2579a14822a9906bf8d9fc9fa69ed7dbdc4991
NilsOmDev <121110418+NilsOmDev@users.noreply.github.com>
2025-12-13 12:29:40 +0100

    Fixed build

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Aufträge / Einsätze werden automatisiert anhand einer Bibliothek erzeugt und a
 Als Simulation können so die Empfänger in der Fireboard Schnittstelle den Auftrag entgegennehmen und bearbeiten.
 Der Benutzer dieser Anwendung kann, währrenddessen die Einsätze mit dokumentieren und mit weiteren Informationen sowie alarmierten Fahrzeugen hinterlegen.
 Die Anwendung dient so im Bereich der Simulation und dem Training von Großschadenslagen mit dem ELW1 / ELW2 in einem Kontext von Hilfsorganisationen.
+
+# Zum kompilieren:
+- Venv mit streamlit und streamlit_autorefresh (venv\Scripts\activate)
+- pyinstaller run.spec --clean --noconfirm
--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import streamlit.components.v1 as components
+
+def init_autorefresh():
+    """Initialisiert den Streamlit-Autorefresh-Component sicher unter PyInstaller."""
+    # Pfad zu den Component-Assets bestimmen
+    if hasattr(sys, '_MEIPASS'):
+        # Onefile → Assets sind im _MEIPASS entpackt
+        component_root = os.path.join(sys._MEIPASS, "streamlit_autorefresh", "frontend", "build")
+    else:
+        # Normal oder onedir – Pfad zum installierten Package
+        import streamlit_autorefresh
+        component_root = os.path.join(os.path.dirname(streamlit_autorefresh.__file__), "frontend", "build")
+
+    # Component registrieren
+    return components.declare_component("st_autorefresh", path=component_root)
+
+# GLOBALE FUNKTION exportieren
+st_autorefresh = init_autorefresh()
+
 import streamlit as st
 
 from backend.Settings_Manager import load_settings
@@ -16,4 +38,3 @@ if(global_vars.debug_mode):
 st.header("Menu")
 st.page_link("pages/settings.py", label="Einstellungen", icon="⚙️")
 st.page_link("pages/task_table.py", label="Einsatzverwaltung", icon="🚨")
-
